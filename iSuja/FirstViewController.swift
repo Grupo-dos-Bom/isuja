147d2578c621294ad0b3c84f2691c8bcae1da5c0
Susin Matheus <mathy@Susin-MBPro.local>
2015-04-14 13:47:50 -0300

    Falafel

---
+++ iSuja/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iSuja
+//
+//  Created by Susin Matheus on 4/14/15.
+//  Copyright (c) 2015 Grupo dos Bom. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
