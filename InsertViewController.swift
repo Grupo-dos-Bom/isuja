d36dbf956da76a46c9c72b231c86ab6ed2fb3a54
Davi Rodrigues <davirdgs@MacBook-Pro-de-Davi.local>
2015-04-15 15:36:52 -0300

    bn

---
+++ InsertViewController.swift
@@ -0,0 +1,14 @@
+//
+//  InsertViewController.swift
+//  iSuja
+//
+//  Created by Davi Rodrigues on 15/04/15.
+//  Copyright (c) 2015 Grupo dos Bom. All rights reserved.
+//
+
+import UIKit
+
+class InsertViewController: UIViewController {
+    
+    
+}
