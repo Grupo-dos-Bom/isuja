2ae62a95a48dd724de7faa3a8d1b587020e6d05c
Diego Silva de Carvalho <diegoak.9@gmail.com>
2015-04-15 13:50:08 -0300

    Wardrobe

---
+++ iSuja/WardrobeCell.swift
@@ -0,0 +1,30 @@
+//
+//  WardrobeCell.swift
+//  iSuja
+//
+//  Created by Diego Silva de Carvalho on 15/04/15.
+//  Copyright (c) 2015 Grupo dos Bom. All rights reserved.
+//
+
+import UIKit
+
+class WardrobeCell: UITableViewCell {
+
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+      // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
